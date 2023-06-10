22abd19638b834235f0d243ff7d34f3537078b4c
Amandeep Singh <contactamandeep18@gmail.com>
2023-06-10 23:32:34 +0530

    Basic Website completed

---
+++ script.js
@@ -0,0 +1,24 @@
+function redirectToBuildResume() {
+  window.location.href = "build-resume_page.html";
+}
+/*                   FAQ SECTION CODE STARTS HERE  */
+let answers = document.querySelectorAll(".accordion");
+
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      // Close any previously expanded question
+      answers.forEach((otherEvent) => {
+        if (otherEvent !== event && otherEvent.classList.contains("active")) {
+          otherEvent.classList.remove("active");
+        }
+      });
+
+      event.classList.add("active");
+    }
+  });
+});
+
+/*                   FAQ SECTION CODE ENDS HERE  */
