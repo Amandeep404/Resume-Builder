285cb1119df5c329fcae5d7765d9e349fd11158b
Arijit <saha65092@gmail.com>
2023-06-12 20:04:57 +0530

    fix build resume page

--- scripts/script.js
+++ scripts/script.js
@@ -24,18 +24,4 @@ answers.forEach((event) => {
 
 /*                   FAQ SECTION CODE ENDS HERE  */
 
-const toggle = document.getElementById("toggleDark");
-const body = document.querySelector("body");
 
-toggle.addEventListener("click", function () {
-  this.classList.toggle("bi-moon");
-  if (this.classList.toggle("bi-brightness-high-fill")) {
-    body.style.background = "whitesmoke";
-    body.style.color = "black";
-    body.style.transition = "2s";
-  } else {
-    body.style.background = "#1d1d1d";
-    body.style.color = "white";
-    body.style.transition = "2s";
-  }
-});
